5fdf43320bd83eb90123eb75eeb3bf5835332af3
hoangff2708 <23520535@gm.uit.edu.vn>
2025-04-21 22:17:45 +0700

    update project

--- src/main/java/com/javaweb/api/BuildingAPI.java
+++ src/main/java/com/javaweb/api/BuildingAPI.java
@@ -15,7 +15,8 @@ public class BuildingAPI {
 	private BuildingService buildingService;
 	@GetMapping(value = "/api/building/")
 	public List<BuildingDTO> getBuilding(@RequestParam(name = "name", required = false) String name,
-	                                     @RequestParam(name = "districtId", required = false) Long district){
+	                                     @RequestParam(name = "districtId", required = false) Long district,
+	                                     @RequestParam(name = "typeCode", required = false) Long typeCode){
 		List<BuildingDTO> result = buildingService.findAll(name);
 		return result;
 	}
